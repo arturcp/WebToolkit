ae56002a98f01cbdb4a6064b5599dcc25625752d
Artur <artur.prado@abril.com.br>
2011-09-09 15:32:18 -0300

    SafeConverter

---
+++ WebToolkit/Converter/SafeConvert.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebToolkit.Converter
+{
+    public static class SafeConvert
+    {
+        public static bool ToBool(object value)
+        {
+            try{return Convert.ToBoolean(value);}catch{return false;}
+        }
+    }
+}
